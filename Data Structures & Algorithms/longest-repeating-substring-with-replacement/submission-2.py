190503e799211480c988b9934edfd85577e74d43
Jimmyniu97 <niuyuefeng@live.cn>
2026-08-09 23:08:01 -0400

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        ans = 0
+        maxF = 0
+        cache = defaultdict(int)
+        for right in range(len(s)):
+            cache[s[right]] += 1
+            maxF =  max(maxF, cache[s[right]])
+            while (right-left+1) - maxF > k:
+                cache[s[left]] -= 1
+                left += 1
+            ans = max(ans, right-left+1)
+        
+        return ans
