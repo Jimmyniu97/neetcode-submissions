794624b40818dcda877fbea579e0b7501cbc4661
Jimmyniu97 <niuyuefeng@live.cn>
2026-09-02 11:58:47 -0400

    Add: search-for-word-ii - submission-2

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-2.py
@@ -0,0 +1,49 @@
+class TrieNode():
+    def __init__(self):
+        self.children = dict()
+        self.end = False
+    
+    def add(self, word):
+        node = self
+        for character in word:
+            if character not in node.children:
+                node.children[character] = TrieNode()
+            node = node.children[character]
+        node.end = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for word in words:
+            root.add(word)
+        
+        ROWS, COLS = len(board), len(board[0])
+        res, visited = set(), set()
+
+        def dfs(i, j, node, word):
+            if (i < 0 or i >= ROWS or j < 0 or
+             j >= COLS or (i, j) in visited or
+              board[i][j] not in node.children):
+              return
+            
+            visited.add((i, j))
+            node = node.children[board[i][j]]
+            word += board[i][j]
+            if node.end:
+                res.add(word)
+            
+            dfs(i + 1, j, node, word)
+            dfs(i - 1, j, node, word)
+            dfs(i, j + 1, node, word)
+            dfs(i, j - 1, node, word)
+            visited.remove((i, j))
+        
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                dfs(i, j, root, "")
+        
+        return list(res)
+
+
+
