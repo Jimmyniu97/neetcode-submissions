615a54a16f590de44ba930246e39361dd9675bd8
Jimmyniu97 <niuyuefeng@live.cn>
2026-09-16 16:30:41 -0400

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        cache = {n: 1}
+
+        def dfs(i):
+            if i in cache:
+                return cache[i]
+            if s[i] == '0':
+                return 0
+            
+            cache[i] = dfs(i+1)
+            if i+1 < n and 10 <= int(s[i:i+2]) <= 26:
+                cache[i] += dfs(i+2)
+            
+            return cache[i]
+
+        return dfs(0)
